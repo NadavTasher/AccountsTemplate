bae1d7c457135db7416edb59135c6b190bf19c0e
Nadav Tasher <tashernadav@gmail.com>
2019-06-06 12:56:18 +0300

    Inherited WAB Update

--- scripts/frontend/utils.js
+++ scripts/frontend/utils.js
@@ -135,7 +135,7 @@ function worker(w = "worker.js") {
     }
 }
 
-function html() {
+function html(callback = undefined) {
     fetch("layouts/template.html", {
         method: "get"
     }).then(response => {
@@ -144,7 +144,8 @@ function html() {
                 method: "get"
             }).then(response => {
                 response.text().then((app) => {
-                    document.body.children[0].innerHTML = template.replace("<!--Your App Body-->", app);
+                    document.body.children[0].innerHTML = template.replace("<!--App Body-->", app);
+                    if (callback !== undefined) callback();
                 });
             });
         });
